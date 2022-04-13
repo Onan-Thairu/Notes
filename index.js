f2163d158b4a7b3ac11970336b16d20e69ee643f
Onan Thairu <thairuonan@gmail.com>
2022-04-13 11:08:45 +0300

    Add Eslint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+//const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -15,79 +15,79 @@ const Note = require('./models/note')
 
 app.get('/api/notes', (request, response) => {
     Note.find({}).then( notes => {
-      response.json(notes)
+        response.json(notes)
     })
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-      .then(note => {
-        if (note) {
-          response.json(note)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(error => next(error))
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response, next) => {
-  const body = request.body
+    const body = request.body
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  })
-  
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
     })
-    .catch(error => next(error))
+
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 
 
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const { content, important } = request.body
+    const { content, important } = request.body
 
-  Note.findByIdAndUpdate(
-    request.params.id, 
-    { content, important },
-     {new: true, runValidators:true, context:'query'}
-     )
-      .then(updatedNote => {
-        response.json(updatedNote)
-      })
-      .catch(error => next(error))
+    Note.findByIdAndUpdate(
+        request.params.id,
+        { content, important },
+        { new: true, runValidators:true, context:'query' }
+    )
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 
 })
 
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint'})
+    response.status(404).send({error: 'unknown endpoint'})
 }
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+    console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({error:'Malformatted id'})
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message})
-  }
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error:'Malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
-  next(error)
+    next(error)
 }
 
 app.use(errorHandler)
